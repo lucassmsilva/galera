3035efec136ecfb8d824ab8c3e64985b3cc0de44
Lucas dos Santos <luca.stos@hotmail.com>
2021-04-09 13:52:18 -0300

    feat: routes and splash add

---
+++ android/app/src/main/kotlin/com/example/galera/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.galera
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:galera/router_generator.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Initially display FirstPage
+      initialRoute: '/splash',
+      onGenerateRoute: RouteGenerator.generateRoute,
+    );
+  }
+}
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    Future.delayed(Duration(seconds: 3)).then((_) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => MyHomePage()));
+    });
+  }
+
+  final mycolor = const Color(0xFF193283);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: mycolor,
+        child: Center(
+          child: Container(
+            width: 150,
+            height: 150,
+            child: Image.asset("assets/logo.png"),
+          ),
+        ));
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          // Within the `FirstScreen` widget
+          onPressed: () {
+            // Navigate to the second screen using a named route.
+            Navigator.pushNamed(context, '/loginpage');
+          },
+          child: Text('Launch screen'),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 150.0,
+            width: 190.0,
+            padding: EdgeInsets.only(top: 40),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(200),
+            ),
+            child: Center(
+              child: Image.asset('assets/logo.png'),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'User Name',
+                  hintText: 'Enter valid mail id as abc@gmail.com'),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  hintText: 'Enter your secure password'),
+            ),
+          ),
+          FlatButton(
+            onPressed: () {
+              //TODO FORGOT PASSWORD SCREEN GOES HERE
+            },
+            child: Text(
+              'Forgot Password',
+              style: TextStyle(color: Colors.blue, fontSize: 15),
+            ),
+          ),
+          Container(
+            height: 50,
+            width: 250,
+            decoration: BoxDecoration(
+                color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+            child: FlatButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/');
+              },
+              child: Text(
+                'Login',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 130,
+          ),
+          Text('New User? Create Account')
+        ],
+      ),
+    );
+  }
+}
